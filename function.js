ee256795ff2cd7bfeab7015b1dfe58de192627f2
olivvinni <128433469+olivvinni@users.noreply.github.com>
2025-03-06 19:10:42 -0300

    Gastronomia Hoje - Projeto no Curso Tecnico
    Site sobre gastronomia em html , css e js .

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
-function openNav() {
-     document.getElementById("mySidenav").style.width = "200px";
-     document.getElementById("main").style.marginLeft = "0px";
- }
- function closeNav() {
-     document.getElementById("mySidenav").style.width = "0";
-     document.getElementById("main").style.marginLeft = "0";
- }
+document.onreadystatechange = function(){
+    var toggleMenu = function(){//função para abrir e fechar o menu
+        var body = document.getElementsByTagName('body')[0];
+        var listaDeClasses = body.classList;//obterlista de clases
+        listaDeClasses.toggle('navAberto');//se tiver a classe navAberto tirar, caso contrario adicionar
+    };
+    document.getElementById('menu-btn').onclick = toggleMenu;//botao menu
+    document.getElementById('auxiliar-menu').onclick = toggleMenu;//elemento auxiliar para fechar quando o usuario clickar fora da nav (mobile e telas de tamanho médio)
+}
